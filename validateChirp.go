a6f71c487053bfe79d94615b8fc1db18eb16f827
Florian Schuett <flo.schuett@chello.at>
2025-06-22 12:07:40 +0200

    Change validateChirp api to clean bad words from the body

--- validateChirp.go
+++ validateChirp.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+type setOfString = map[string]struct{}
 
 const maxChirpLength = 140
 
@@ -12,7 +15,7 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -22,12 +25,32 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if len(params.Body) > maxChirpLength {
+	msg := params.Body
+	if len(msg) > maxChirpLength {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 
+	badWords := setOfString{
+		"kerfuffle": {},
+		"sharbert": {},
+		"fornax": {},
+	}
+
 	respondWithJson(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleanWords(msg, badWords),
 	})
+}
+
+func cleanWords(msg string, wordsToExclude setOfString) string {
+	const delimiter = " "
+	words := strings.Split(msg, delimiter)
+
+	for i, word := range words {
+		if _, exists := wordsToExclude[strings.ToLower(word)]; exists {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, delimiter)
 }
